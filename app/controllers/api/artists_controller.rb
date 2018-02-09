9c2381f2ccd55a9ce7affaedc4a39c96b2d6425d
DenimCity <altidorj1@gmail.com>
2018-02-09 12:34:19 -0500

    routes are defined

--- app/controllers/api/artists_controller.rb
+++ app/controllers/api/artists_controller.rb
@@ -6,5 +6,41 @@ class Api::ArtistsController < ApplicationController
 render json: @artists
   end
 
+  def show
+    @artist = Artist.find(params[:id])
+    render json: @artist
+
+  end
+  
+  def create
+    @artist = Artist.create!(artist_params)
+    render json: @artist
+
+  end
+  
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.update.!(artist_params)
+    render json: @artist
+
+  end
+  
+  def destroy
+    @artist = Arist.find(params[:id])
+    @artist.destroy
+    render json:@artist
+
+    render status: :ok
+    
+
+  end
+
+
+private
+def artist_params
+    params.require(:artist)
+    .permit(:name,:photo_url, :nationality)
+end
+
 
 end
